6d153f85cf781ce53e4398a5474f408cc49354a5
Biswareet Panda <biswareetpanda@Biswareets-MacBook-Air.local>
2025-06-18 20:53:38 +0530

    Fix sha256 after verifying

--- Formula/asitop-csv-logger.rb
+++ Formula/asitop-csv-logger.rb
@@ -2,7 +2,7 @@ class AsitopCsvLogger < Formula
   include Language::Python::Virtualenv
 
   desc "CSV logger for ASITOP on Apple Silicon"
-  homepage "https://github.com/Intruder2614/homebrew-asitop-csv-logger"
+  homepage "https://github.com/Intruder2614/asitop-csv-logger"
   url "https://github.com/Intruder2614/asitop-csv-logger/releases/download/v1.1.0.8/asitop_csv_logger-1.0.8.tar.gz"
 
   sha256 "5bcfa0461f567bbdb06ee91a2ad81a4a423a0ad0beebbffcd861727ad6fa2fc7"
